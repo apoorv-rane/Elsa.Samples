b33d937388b9386632263d27407a78cb48eaa122
alfaiz12 <alfaizkhan147@gmail.com>
2021-11-09 14:44:46 +0530

    Added all files

---
+++ Api/OrderFilling/OrderFilling.Entities/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderFilling.Entities
+{
+    public class Order
+    {
+        [Key]
+        public Guid OrderId { get; set; }
+        public string ProductId { get; set; } 
+        public DateTime OrderDateTime { get; set; }
+        public bool DeliveryStatus { get; set; }
+        public string DeliveryBy { get; set; }
+        public int Quantity { get; set; }
+    }
+}
