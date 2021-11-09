0fa41c18796eab48d115ea47ac04ff12178d139b
Shivam <gupta.shivam@neosoftmail.com>
2021-11-09 18:20:52 +0530

    Added HireEmployee Project

---
+++ Api/HireEmployee/HireEmployee.Entities/Entities/Candidate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireEmployee.Entities
+{
+    public class Candidate
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid JobId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public string Skill { get; set; }
+        public int Experience { get; set; }
+        public string Resume { get; set; }
+        public int ExpectedSalary { get; set; }
+    }
+}
---
+++ Api/HireEmployee/HireEmployee.Repositories/Repositories/JobRepository.cs
@@ -0,0 +1,17 @@
+﻿using HireEmployee.Entities;
+using HireEmployee.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireEmployee.Repositories.Repositories
+{
+    public class JobRepository : BaseRepository<Job>, IJobRepository
+    {
+        public JobRepository(HireEmployeeDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
