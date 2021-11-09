883850ba8b25b21a3d39499956ec9347d5fbddef
dwivedi-ankita <ankita.dwivedi@neosoftmail.com>
2021-11-09 14:54:47 +0530

    New API added

---
+++ Api/NewProjectApprovalAPI/IRepositories/IProjectRepository.cs
@@ -0,0 +1,13 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRepositories
+{
+    public interface IProjectRepository : IAsyncRepository<Project>
+    {
+        //Task<CreateProject> AddProject(CreateProject project);
+    }
+}
---
+++ Api/NewProjectApprovalAPI/Repositories/ProjectDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repositories
+{
+    public class ProjectDbContext : DbContext
+    {
+        public ProjectDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Project> Projects { get; set; }
+
+    }
+}
