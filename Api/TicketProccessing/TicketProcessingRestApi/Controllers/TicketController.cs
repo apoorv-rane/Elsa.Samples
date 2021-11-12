f5a342d20aba2c536a94c37238bc23c57d314e1d
NikitaBhoir <bhoir.nikita.18ce5504@gmail.com>
2021-11-12 20:17:44 +0530

    controller updated

--- Api/TicketProccessing/TicketProcessingRestApi/Controllers/TicketController.cs
+++ Api/TicketProccessing/TicketProcessingRestApi/Controllers/TicketController.cs
@@ -22,6 +22,7 @@ namespace TicketProcessingRestApi.Controllers
             this.ticketRepository = _ticketRepository;
         }
 
+
         [HttpGet("GetAllTickets")]
         public async Task<ActionResult> GetAllUsers()
         {
@@ -31,28 +32,40 @@ namespace TicketProcessingRestApi.Controllers
 
 
 
-        [HttpPost("AddTicket")]
-        public async Task<Ticket> Create(Ticket ticketEntity)
+        [HttpGet("GetTicket")]
+        public async Task<ActionResult> GetTicketById(string id)
         {
-            ticketEntity.CreateDate = DateTime.Now;
+            var ticketId = new Guid(id);
+            var TicketDetails = await ticketRepository.GetByIdAsync(ticketId);
 
-            var response = await ticketRepository.AddAsync(ticketEntity);
-            /* using(HttpClient httpClient = new HttpClient())
-             {
-                 string JsonResult = JsonConvert.SerializeObject(response);
-                 StringContent stringContent = new StringContent(JsonResult, System.Text.Encoding.UTF8, "application/json");
-                 HttpResponseMessage httpResponse = httpClient.PostAsync("", httpContent).Result);
-             }*/
-
-            return response;
+            return Ok(TicketDetails);
         }
 
+
+
+        [HttpPost("AddTicket")]
+        public async Task<ActionResult> Create(Ticket ticketEntity)
+        {
+            ticketEntity.CreateDate = DateTime.Now;
+            var response = await ticketRepository.AddAsync(ticketEntity);
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string JsonResult = JsonConvert.SerializeObject(response);
+                StringContent stringContent = new StringContent(JsonResult, System.Text.Encoding.UTF8, "application/json");
+                HttpResponseMessage httpResponse = httpClient.PostAsync("https://localhost:5001/PostTicket", stringContent).Result;
+            }
+            return Ok(response);
+        }
+
+
+
+
         [HttpPut("assignManager")]
-        public async Task<ActionResult> AssignTicketToMAnager(Ticket ticketEntity)
+        public async Task<ActionResult> AssignTicketToManager(Ticket ticketEntity)
         {
             TimeSpan start = new TimeSpan(10, 0, 0); //10am o'clock
             TimeSpan end = new TimeSpan(20, 0, 0); //8pm o'clock
-
             TimeSpan now = DateTime.Now.TimeOfDay;
 
             if (start <= end)
@@ -62,50 +75,103 @@ namespace TicketProcessingRestApi.Controllers
                     switch (ticketEntity.TicketType)
                     {
                         case "IT":
-                            ticketEntity.assignManager = "vaibhav.kulkarni@gmail.com";
+                            ticketEntity.assignManager = "vaibhav.kulkarni@neosoftmail.com";
                             break;
                         case "HR":
-                            ticketEntity.assignManager = "simrangmail.com";
+                            ticketEntity.assignManager = "simran@neosoftmail.com";
                             break;
                         case "Admin":
-                            ticketEntity.assignManager = "vaibhav.kulkarni@gmail.com";
+                            ticketEntity.assignManager = "tushar@neosoftmail.com";
+                            break;
+                    }
+
+                }
+                else
+                {
+                    switch (ticketEntity.TicketType)
+                    {
+                        case "IT":
+                            ticketEntity.assignManager = "neha.haridas@neosoftmail.com";
+                            break;
+                        case "HR":
+                            ticketEntity.assignManager = "Shradha@neosoftmail.com";
+                            break;
+                        case "Admin":
+                            ticketEntity.assignManager = "bhavesh@neosoftmail.com";
                             break;
                     }
                 }
             }
-            else
-            {
-                switch (ticketEntity.TicketType)
-                {
-                    case "IT":
-                        ticketEntity.assignManager = "neha.haridas@gmail.com";
-                        break;
-                    case "HR":
-                        ticketEntity.assignManager = "Shradha@gmail.com";
-                        break;
-                    case "Admin":
-                        ticketEntity.assignManager = "bhavesh@gmail.com";
-                        break;
-                }
-
-            }
 
             await ticketRepository.UpdateAsync(ticketEntity);
-            return Ok();
+            return Ok(ticketEntity);
         }
+
+
         [HttpPut("AssignTicket")]
         public async Task<ActionResult> AssignTickettoPerson(Ticket ticketEntity)
         {
             ticketEntity.assignDate = DateTime.Now;
+            if (ticketEntity.assignPerson == "")
+            {
+                TimeSpan start = new TimeSpan(10, 0, 0); //10am o'clock
+                TimeSpan end = new TimeSpan(20, 0, 0); //8pm o'clock
+                TimeSpan now = DateTime.Now.TimeOfDay;
+
+                if (start <= end)
+                {
+                    if ((now >= start) && (now <= end))
+                    {
+                        switch (ticketEntity.TicketType)
+                        {
+                            case "IT":
+                                ticketEntity.assignPerson = "vina.patil@neosoftmail.com";
+                                break;
+                            case "HR":
+                                ticketEntity.assignPerson = "vishal@neosoftmail.com";
+                                break;
+                            case "Admin":
+                                ticketEntity.assignPerson = "Vaibhav@neosoftmail.com";
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        switch (ticketEntity.TicketType)
+                        {
+                            case "IT":
+                                ticketEntity.assignPerson = "akshay@neosoftmail.com";
+                                break;
+                            case "HR":
+                                ticketEntity.assignPerson = "bhavesh@neosoftmail.com";
+                                break;
+                            case "Admin":
+                                ticketEntity.assignPerson = "bhavna@neosoftmail.com";
+                                break;
+                        }
+                    }
+                }
+            }
             await ticketRepository.UpdateAsync(ticketEntity);
-            return Ok();
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string JsonResult = JsonConvert.SerializeObject(ticketEntity);
+
+                StringContent stringContent = new StringContent(JsonResult, System.Text.Encoding.UTF8, "application/json");
+                httpClient.DefaultRequestHeaders.Add("X-Correlation-Id", ticketEntity.TicketId.ToString());
+                HttpResponseMessage httpResponse = httpClient.PutAsync("https://localhost:5001/PutTicket", stringContent).Result;
+            }
+            return Ok(ticketEntity);
         }
-        [HttpPut("IsResloved")]
+
+
+
+        [HttpPut("IsIssueResloved")]
         public async Task<ActionResult> IssueResolve(Ticket ticketEntity)
         {
             ticketEntity.isResolved = true;
             await ticketRepository.UpdateAsync(ticketEntity);
-            return Ok();
+            return Ok(ticketEntity);
         }
     }
 }
